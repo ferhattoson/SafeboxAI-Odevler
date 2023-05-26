30a2c4632dc9dc70075705517faa673d5155743b
ferhat toson <105549351+ferhattoson@users.noreply.github.com>
2023-05-26 15:59:18 +0300

    Delete odev_3 directory

--- odev_3/LAG-1.sql
+++
@@ -1,7 +0,0 @@
-
-Select category.id, COUNT(*) AS count,
-       LAG(COUNT(*)) over (order by COUNT(*) ) as previous_count
-From film_category 
-join category on film_category.category_id = category.category_id
-group by category.id
-order by count ;
